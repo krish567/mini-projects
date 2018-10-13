38d5bdda68c4a88948d9ea14e6518bdd569cc9e5
KRISHNA CHAITANYA KALUVA <kaluvakrishnachaitanya@gmail.com>
2018-10-14 00:13:30 +0530

    adding automatic conflunce documentation from python

--- export_to_confluence.py
+++ export_to_confluence.py
@@ -1,2 +1,7 @@
 import numpy as np
 import os, shutil
+import json
+js = {"type":"page","title":"Testing REST API","space":{"key":"~krish"}, "body":{"storage":{"value":"<p>This is the information that is written automatically to this page</p>","representation":"storage"}}}
+os.system("curl -u krish:55747462 -X POST -H 'Content-Type: application/json' -d'"+ json.dumps(js) +"' http://192.168.2.148/confluence/rest/api/content/ | python -mjson.tool")
+
+# curl -u krish:55747462 -X POST -H 'Content-Type: application/json' -d'{"type":"page","title":"Testing REST API","space":{"key":"~krish"}, "body":{"storage":{"value":"<p>This is the information that is written automatically to this page</p>","representation":"storage"}}}' http://192.168.2.148/confluence/rest/api/content/ | python -mjson.tool
